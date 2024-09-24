322d3bf051b2ef742abf462c11c9bfd536c5ecad
Eric Mescouto <h.ericmescouto@gmail.com>
2024-09-23 23:02:51 -0300

    :sparkles: feat: adiciona a função de divisão.

--- calculadora.js
+++ calculadora.js
@@ -1,8 +1,11 @@
 import adicao from "./adicao.js";
+import divisao from "./divisao.js";
 import subtracao from "./subtracao.js";
 
 let resultadoAdicao = adicao(2, 5);
 let resultadoSubtracao = subtracao(5, 3);
+let resultadoDivisao = divisao(10, 5);
 
 console.log(resultadoAdicao);
 console.log(resultadoSubtracao);
+console.log(resultadoDivisao);
