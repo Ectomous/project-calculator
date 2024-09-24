91a538827c0d666e5e6e17a0a39968d4c6ea99c5
Eric Mescouto <h.ericmescouto@gmail.com>
2024-09-23 23:04:47 -0300

    :sparkles: feat: adiciona a função de multiplicação.

--- calculadora.js
+++ calculadora.js
@@ -1,11 +1,14 @@
 import adicao from "./adicao.js";
 import divisao from "./divisao.js";
 import subtracao from "./subtracao.js";
+import multiplicacao from "./multiplicacao.js";
 
 let resultadoAdicao = adicao(2, 5);
 let resultadoSubtracao = subtracao(5, 3);
 let resultadoDivisao = divisao(10, 5);
+let resultadoMultiplicacao = multiplicacao(3, 3);
 
 console.log(resultadoAdicao);
 console.log(resultadoSubtracao);
 console.log(resultadoDivisao);
+console.log(resultadoMultiplicacao);
